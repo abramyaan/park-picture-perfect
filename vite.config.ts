0c56cbb7ae8d8d5f61f80c17aec7cd849da13f26
ero <abramyan_ero@mail.ru>
2026-03-01 13:29:08 +0500

    add deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Укажи здесь имя своего репозитория в кавычках, например: base: "/my-park-project/",
+  // Если будешь привязывать свой домен (vkadre.ru), оставь просто "/"
+  base: "/", 
+  
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +15,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
